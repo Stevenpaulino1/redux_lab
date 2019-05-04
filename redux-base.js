4d2e1bc2701ece30a6e7d6222b3800effda9d004
Steven Paulino <stevenpaulino1@hotmail.com>
2019-05-04 17:55:49 -0400

    basic redux flow

---
+++ redux-base.js
@@ -0,0 +1,32 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+};
+
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
+  return state;
+};
+
+store.subscribe(() => {
+  console.log("subscribe", store.getState());
+});
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
