078016718f9036ea8c885b4844832aa39c9da3e2
Steven Paulino <stevenpaulino1@hotmail.com>
2019-05-10 14:41:05 -0400

    store results

---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+  counter: 0,
+  results: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, counter: state.counter + 1 };
+
+    case "DECREMENT":
+      return { ...state, counter: state.counter - 1 };
+
+    case "ADD":
+      return { ...state, counter: state.counter + action.payload };
+    case "SUBTRACT":
+      return { ...state, counter: state.counter - action.payload };
+    case "STORE_RESULT":
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: state.counter })
+      };
+  }
+
+  return state;
+};
+
+export default reducer;
